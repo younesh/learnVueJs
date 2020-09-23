d0900df349c331f79c4b79d03b04b2052b42f1d1
younesh <younesharke@gmail.com>
2020-09-23 12:33:03 +0200

    next formation udemy vue test ...

---
+++ udemy-en-courses/env-dev/app01-test/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <template v-for="item in 3">
+         
+        <div :key="item" v-show="(false)">  {{ clsItem = "MyCpt--style0" +  item }} </div>  <!--  c 'est une bidouill perso pour pouvoire affecter un idex de for dynamiquement au non de class ...  -->
+        <my-alias-cpt  :msg="clsItem" :cls="clsItem" :key="item"/>
+    </template>
+  </div>
+</template>
+
+<script>
+import MyCpt from './components/MyCpt'  
+export default {
+  name: 'App',
+  components: {
+              "my-alias-cpt" : MyCpt  // en peut creer un alias inclure notre cpt fils !! 
+  }
+
+}
+</script>
+
+<style lang="scss">
+@import "@/components/config/scss/_variables.scss";
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ udemy-en-courses/env-dev/app01-test/src/components/MyCpt.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="MyCpt" :class="cls">
+        <p>{{ msg }} : {{ status }}</p>
+        <button @click="changeStatus"> change status </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'MyCpt', // name -> @1
+    props: {
+        msg : String,
+        cls : String
+    },
+    data() {
+    return {
+        status : "No critical !! "
+    }
+  },
+    methods : {
+    changeStatus : function () {
+        this.status = "normal";
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/components/config/scss/_variables.scss";
+ .MyCpt { 
+     padding: $gutter-half;
+     margin:  $gutter-half;
+ //background-color: $color01;
+     &--style01 {  // .MyCpt-style01 
+          background-color: $color01;
+     }
+     &--style02 {  // .MyCpt-style02 
+          background-color: $color02;
+     }
+     &--style03 {  // .MyCpt-style03
+          background-color: $color03;
+     } 
+ }
+
+     /*   .MyCpt--style01 {  // .MyCpt-style01 
+          background-color: $color01;
+     }
+     .MyCpt--style02 {  // .MyCpt-style02 
+          background-color: $color02;
+     } */
+</style>
